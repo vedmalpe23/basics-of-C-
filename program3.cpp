5be44da1f313a0c7d4f7f324d135cc989af78b46
ved malpe <vedmalpe21@gmail.com>
2025-07-31 22:36:31 +0530

    Create program3.cpp

---
+++ program3.cpp
@@ -0,0 +1,23 @@
+//program3:calculator program
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num1,num2;
+    cout<<"enter first number: ";
+    cin>>num1;
+    cout<<"enter second number: ";
+    cin>>num2;
+    cout<<"addition of two numbers: ";
+    int add=num1+num2;
+    cout<<add<<endl;
+    cout<<"subtraction of two numbers: ";
+    int sub=num1-num2;
+    cout<<sub<<endl;
+    cout<<"multiplication of two numbers: ";
+    int mul=num1*num2;
+    cout<<mul<<endl;
+    cout<<"division of two numbers: ";
+    float div=(num1/num2);
+    cout<<div<<endl;
+}
