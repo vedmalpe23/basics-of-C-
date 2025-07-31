9bd4fe8f9b443587f606ada96b773d0edfacb561
ved malpe <vedmalpe21@gmail.com>
2025-07-31 22:33:39 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,11 @@
+//program2:inputting two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter first number: ";
+    cin>>num1;
+    cout<<"enter second number: ";
+    cin>>num2;
+}
